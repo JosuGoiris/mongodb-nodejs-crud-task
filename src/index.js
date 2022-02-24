789591005a269daafa57afb75072fa71243b6adf
JosuGoiris <josugoirisitaipu@gmail.com>
2022-02-24 14:12:07 -0300

    dalepy

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ import app from "./app";
 import "./database";
 import { PORT } from "./config";
 
-app.listen(PORT);
+const server = app.listen(PORT, () => {
+  console.log("server started");
+});
+process.on("SIGTERM", () => {
+  server.close(() => {
+    setTimeout(() => {
+      process.exit(0);
+    }, 3000);
+  });
+});
 
 console.log("Server on port", PORT);
